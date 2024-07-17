02024235285739297406a61a4333f5399c4ecccd
murilogillbert <murilo.gillbert@outlook.com>
2024-07-17 13:16:11 -0400

    Pequenas mudanças.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 export default function Navbar() {
     
+    const redireciona = () => {
+        window.location.href('https://www.w3schools.com/js/js_window_location.asp')
+    }
+
     return(
         <>
-            
+            <div className="logo">
+                <img src='../../assets/LOGO_DARK.png'></img>
+            </div>
+            <div className='menu-itens'>
+
+            </div>
+            <div>
+                <input type="button" onClick={redireciona()}>Registrar</input>
+            </div>
         </>
     )
 }
